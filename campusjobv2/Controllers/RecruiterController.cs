102af9b7af9072740c4cf519932dd7c139b67a5f
Deeshan Ranathunga <deeshanranathunga2002@gmail.com>
2025-02-25 17:11:16 +0000

    Bare bone setup to start clean.

---
+++ campusjobv2/Controllers/RecruiterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace campusjobv2.Controllers
+{
+    public class RecruiterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
