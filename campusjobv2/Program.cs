188f457d9c4c778a1842440eccae157b6258b262
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-03-24 13:51:05 +0000

    Update Program.cs

--- campusjobv2/Program.cs
+++ campusjobv2/Program.cs
@@ -3,16 +3,16 @@ using campusjobv2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-    new MySqlServerVersion(new Version(8, 0, 34)))); 
+    new MySqlServerVersion(new Version(8, 0, 34)));
 
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -29,4 +29,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
