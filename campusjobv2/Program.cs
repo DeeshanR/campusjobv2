c03a6203b1f170d12900c599b29e3546a516a790
billywebstershu <167339854+billywebstershu@users.noreply.github.com>
2025-04-14 14:11:27 +0100

    Update Program.cs

--- campusjobv2/Program.cs
+++ campusjobv2/Program.cs
@@ -4,17 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllersWithViews();
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 34))
-        )
+    )
 );
-
 
 builder.Services.AddSession(options =>
 {
@@ -25,10 +22,7 @@ builder.Services.AddSession(options =>
     options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
 });
 
-
-
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -39,8 +33,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-
 app.UseSession();
 
 using (var scope = app.Services.CreateScope())
@@ -49,81 +41,19 @@ using (var scope = app.Services.CreateScope())
     try
     {
         var context = services.GetRequiredService<ApplicationDbContext>();
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        
-
         context.Database.EnsureCreated();
-        
-        if (!context.Users.Any())
-        {
-
-            
-
-            var adminUser = new User
-            {
-                First_Name = "Admin",
-                Last_Name = "User",
-                Email = "admin@campusjobs.com",
-                Password = "test",
-                Role = 1
-            };
-            context.Users.Add(adminUser);
-            
-
-            var recruiterUser = new User
-            {
-                First_Name = "John",
-                Last_Name = "Recruiter",
-                Email = "recruiter@campusjobs.com",
-                Password = "test",
-                Role = 2
-            };
-            context.Users.Add(recruiterUser);
-            
-
-            var studentUser = new User
-            {
-                First_Name = "Alice",
-                Last_Name = "Student",
-                Email = "student@campusjobs.com",
-                Password = "test",
-                Role = 3
-            };
-            context.Users.Add(studentUser);
-            
-            context.SaveChanges();
-            
-
-            context.Admins.Add(new Admin { User_ID = adminUser.User_ID });
-            
-
-            var recruiter = new Recruiter { User_ID = recruiterUser.User_ID };
-            context.Recruiters.Add(recruiter);
-            
-
-            context.Employees.Add(new Employee 
-            { 
-                Student_ID = 1001, 
-                Recruitment_ID = recruiter.Recruitment_ID 
-            });
-            
-            context.SaveChanges();
-
-        }
     }
     catch (Exception ex)
     {
-        
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while creating the database.");
     }
 }
-
 
 app.Use(async (context, next) =>
 {
     var path = context.Request.Path;
     var method = context.Request.Method;
-    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-    
 
     var excludedPaths = new[] {
         "/Login",
@@ -145,7 +75,6 @@ app.Use(async (context, next) =>
     }
 
     var userId = context.Session.GetInt32("UserId");
-
     
     if (userId == null)
     {
